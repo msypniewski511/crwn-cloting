b7332c9bf59b5ba5af688407dbe5042f1624bf51
Maciej Sypniewski <msypniewski511@gmail.com>
2020-01-16 21:19:44 +0000

    Updates

---
+++ precache-manifest.20c8814daa812d3721d00c00030a8ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b8bef63b7cb9e89d765b6ead4afeb8",
+    "url": "/crwn-cloting/index.html"
+  },
+  {
+    "revision": "5c91f0ca93eb506140e5",
+    "url": "/crwn-cloting/static/css/main.7500ff18.chunk.css"
+  },
+  {
+    "revision": "b05b17230e2629208ada",
+    "url": "/crwn-cloting/static/js/2.741c266d.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/crwn-cloting/static/js/2.741c266d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c91f0ca93eb506140e5",
+    "url": "/crwn-cloting/static/js/main.ff297024.chunk.js"
+  },
+  {
+    "revision": "c25ae4ac0435af072d94",
+    "url": "/crwn-cloting/static/js/runtime-main.e8e2ad5f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-cloting/static/media/crown.ed28accb.svg"
+  }
+]);
